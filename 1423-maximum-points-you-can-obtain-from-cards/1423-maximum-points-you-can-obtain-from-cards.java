83ddb11ae3bb3de06e668a2c5f52cd4acff297b2
Vignesh C P <vigneshcp109@gmail.com>
2026-09-20 09:18:48 +0530

    Time: 2 ms (99.71%), Space: 61.9 MB (17.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -2,24 +2,42 @@ import java.util.*;
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
     int n=cardPoints.length;
-    int left=0,right=0,totalsum=0,min=0,sum=0;
-    while(right<n){
-    totalsum+=cardPoints[right];
-    right++;
-    }   
-    right=0;
-    while(right<n-k){
-        sum+=cardPoints[right];
-        right++;    
+
+    // taking n-k concept
+    // int left=0,right=0,totalsum=0,min=0,sum=0;
+    // while(right<n){
+    // totalsum+=cardPoints[right];
+    // right++;
+    // }   
+    // right=0;
+    // while(right<n-k){
+    //     sum+=cardPoints[right];
+    //     right++;    
+    // }
+    // min=sum;
+    // while(right<n){
+    // sum+=cardPoints[right];
+    // sum-=cardPoints[left];
+    // right++;
+    // left++;
+    // min=Math.min(min,sum);
+    // }
+    // return totalsum-min;
+
+
+    // Leftsum rightsum concept
+    int leftsum=0,rightsum=0,ans=0;
+    for(int i=0;i<k;i++){
+        leftsum+=cardPoints[i];
     }
-    min=sum;
-    while(right<n){
-    sum+=cardPoints[right];
-    sum-=cardPoints[left];
-    right++;
-    left++;
-    min=Math.min(min,sum);
+    ans=leftsum;
+   int right=n-1;
+    for(int i=k-1;i>=0;i--){
+        leftsum-=cardPoints[i];
+        rightsum+=cardPoints[right];
+        right--;
+        ans=Math.max(ans,leftsum+rightsum);
     }
-    return totalsum-min;
+    return ans;
     }
     }
